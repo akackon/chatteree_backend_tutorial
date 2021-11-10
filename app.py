0efebbf05d65d57c142e67c2066d345c9f32782e
akackon <adrianackon@rocketmail.com>
2021-11-10 08:34:17 +0000

    update pip list

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import chatteree_backend
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Hello World'
+    return "Hello World"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
- 
